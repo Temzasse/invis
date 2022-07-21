bc1c1c208ad165f54687ef748f1cb0686f917bc1
Teemu Taskula <themetor@gmail.com>
2022-07-21 19:12:31 +0300

    Fix health check

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   output: 'standalone',
-}
+  rewrites: async () => {
+    return [{ source: '/healthcheck', destination: '/api/healthcheck' }];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
