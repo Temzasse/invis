bf0baf42c753a90504195b6ab78b92f4deb244b8
Teemu Taskula <themetor@gmail.com>
2023-04-09 12:19:13 +0300

    Fix broken test setup

--- vitest.config.ts
+++ vitest.config.ts
@@ -10,6 +10,6 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'happy-dom',
-    setupFiles: ['./test/setup-test-env.ts'],
+    setupFiles: ['./client/test/setup-test-env.ts'],
   },
 });
