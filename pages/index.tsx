f11fd2b01b6f68627f7b71acebbd2b4020d3a72a
Teemu Taskula <themetor@gmail.com>
2023-05-21 14:28:17 +0300

    FIx landing page redirect

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,8 @@
 import { type GetServerSideProps } from 'next';
 import { motion, AnimatePresence } from 'framer-motion';
 import Head from 'next/head';
+import Link from 'next/link';
+import Image from 'next/image';
 
 import {
   SplashScreen,
@@ -8,9 +10,8 @@ import {
 } from '~components/navigation/SplashScreen';
 
 import { styled } from '~styles/styled';
-import { Button, Stack, Text } from '~components/uikit';
-import Link from 'next/link';
-import Image from 'next/image';
+import { Button, Stack } from '~components/uikit';
+import { getProjectFromCookies } from '~server/utils/project';
 
 export default function Landing() {
   const isSplashVisible = useSplashScreen();
@@ -72,7 +73,7 @@ const Main = styled(motion.main, {
 });
 
 const BackgroundGrid = styled('div', {
-  backgroundImage: `url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 32 32' width='32' height='32' fill='none' stroke='rgb(241 245 249 / 0.03)'%3e%3cpath d='M0 .5H31.5V32'/%3e%3c/svg%3e")`,
+  backgroundImage: `url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 32 32' width='32' height='32' fill='none' stroke='rgb(241 245 249 / 0.1)'%3e%3cpath d='M0 .5H31.5V32'/%3e%3c/svg%3e")`,
   backgroundPosition: 'center top -1px',
   WebkitMaskImage: 'linear-gradient(0deg,#0000,#000)',
   MaskImage: 'linear-gradient(0deg,#0000,#000)',
@@ -101,9 +102,11 @@ const Title = styled('h1', {
 });
 
 export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+  const project = await getProjectFromCookies(req.cookies);
+
   return {
     props: {},
-    redirect: req.cookies.project
+    redirect: project
       ? { destination: '/app/home', permanent: false }
       : undefined,
   };
