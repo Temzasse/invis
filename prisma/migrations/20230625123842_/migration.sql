e755ec5bdea9eb5c56996c7023c922369affa177
Teemu Taskula <themetor@gmail.com>
2023-06-25 16:48:23 +0300

    Add unique constraints to db schema

--- prisma/migrations/20230625123842_/migration.sql
+++ prisma/migrations/20230625123842_/migration.sql
@@ -29,3 +29,12 @@ CREATE TABLE "Category" (
     "updatedAt" DATETIME NOT NULL,
     CONSTRAINT "Category_projectId_fkey" FOREIGN KEY ("projectId") REFERENCES "Project" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
 );
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Project_name_key" ON "Project"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Item_name_categoryId_key" ON "Item"("name", "categoryId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Category_name_projectId_key" ON "Category"("name", "projectId");
