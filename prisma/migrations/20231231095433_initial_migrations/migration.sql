1035d5f9117a44159e962708791498e79f20ff04
Teemu Taskula <themetor@gmail.com>
2023-12-31 11:55:10 +0200

    Squash db migrations

--- prisma/migrations/20231231095433_initial_migrations/migration.sql
+++ prisma/migrations/20231231095433_initial_migrations/migration.sql
@@ -30,6 +30,28 @@ CREATE TABLE "Category" (
     CONSTRAINT "Category_projectId_fkey" FOREIGN KEY ("projectId") REFERENCES "Project" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
 );
 
+-- CreateTable
+CREATE TABLE "ShoplistItem" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "checked" BOOLEAN NOT NULL DEFAULT false,
+    "shoplistId" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    CONSTRAINT "ShoplistItem_shoplistId_fkey" FOREIGN KEY ("shoplistId") REFERENCES "Shoplist" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "Shoplist" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "completed" BOOLEAN NOT NULL DEFAULT false,
+    "projectId" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    "completedAt" DATETIME,
+    CONSTRAINT "Shoplist_projectId_fkey" FOREIGN KEY ("projectId") REFERENCES "Project" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
 -- CreateIndex
 CREATE UNIQUE INDEX "Project_name_key" ON "Project"("name");
 
